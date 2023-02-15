20407c34740491a9aa610231af384b28cbf0b3e2
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2023-02-15 16:32:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//Exersice 1 
+
+let number1 = prompt();
+let number2 = prompt();
+
+let n1 = Number(number1);
+let n2 = Number(number2);
+
+let n3 = n1 + n2;
+if (n3 > 10) {
+    alert("Okay:)");
+}
+else {
+    alert("Погано:(")
+}
+
+//Exersice 2 
+
+let name2 = prompt("Vlad");
+
+if (name2 == "Vlad") {
+    alert("ПРИВІТ")
+}
+else {
+    alert("ХТО ТИ?")
+}
